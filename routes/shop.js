c36ba2034f7b96d0ca0272d2c58320d15ad492bb
Codjam <jamaltbutler>
2018-12-10 23:57:39 -0500

    checking to see if I want to work with pug or ejs

--- routes/shop.js
+++ routes/shop.js
@@ -8,9 +8,12 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products);
-
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    docTitle: 'shop'
+  });
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
